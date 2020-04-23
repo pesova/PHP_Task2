63befe653dff4580b06f3b270e370b21fe6fc816
pesova <62014292+pesova@users.noreply.github.com>
2020-04-23 19:29:35 +0100

    Update header.php

--- Lib/header.php
+++ Lib/header.php
@@ -6,7 +6,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
   </head>
 <body class="container-fluid bg-light">
