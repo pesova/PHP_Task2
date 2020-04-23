fc3803627aa836db83f23ac6ec25fdd9e9386f56
pesova <62014292+pesova@users.noreply.github.com>
2020-04-23 19:02:06 +0100

    Php Task 3

---
+++ Lib/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="bootstrap-4.4.1-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+  </head>
+<body class="container-fluid bg-light">
+    <ul class="nav nav-tabs nav-bar-nav bg-light">
+      <li class="nav-item">
+        <a  class="nav-link" data-toggle="pill" href="index.php">StartNG Hospital</a>
+      <li class="nav-item">
+          <a  class="nav-link" data-toggle="tab" href="index.php">Home</a>
+      </li>
+    </ul>
